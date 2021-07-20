3de3a42adba8dbeea28206776b5e35cc7453e531
Dungna29 <dungna29@fpt.edu.vn>
2021-07-20 09:17:36 +0700

    1

---
+++ 4_IT16313_NET103_BLOCK2_SM21/BAI_1_3_ADO.NET/1_FrmKetNoiCSDL.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace BAI_1_3_ADO.NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_KetNoiCSDL = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_KetNoiCSDL
+            // 
+            this.btn_KetNoiCSDL.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_KetNoiCSDL.Location = new System.Drawing.Point(299, 29);
+            this.btn_KetNoiCSDL.Name = "btn_KetNoiCSDL";
+            this.btn_KetNoiCSDL.Size = new System.Drawing.Size(170, 82);
+            this.btn_KetNoiCSDL.TabIndex = 0;
+            this.btn_KetNoiCSDL.Text = "Kết Nối DB";
+            this.btn_KetNoiCSDL.UseVisualStyleBackColor = true;
+            this.btn_KetNoiCSDL.Click += new System.EventHandler(this.btn_KetNoiCSDL_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 134);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 304);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_KetNoiCSDL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_KetNoiCSDL;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ 4_IT16313_NET103_BLOCK2_SM21/BAI_1_3_ADO.NET/1_FrmKetNoiCSDL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BAI_1_3_ADO.NET
+{
+    public partial class Form1 : Form
+    {
+        private string _sqlConnectionString =
+            @"Data Source=DUNGNA_PC2021\SQLEXPRESS;Initial Catalog=CSharp3_Dungna29;User ID=dungna29;Password=123";
+
+        private SqlConnection _con;
+        private SqlCommand _cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_KetNoiCSDL_Click(object sender, EventArgs e)
+        {
+            //1. Khởi tạo lớp kết nối vào CSDL thông qua đường dẫn
+            _con = new SqlConnection(_sqlConnectionString);
+            //2. Mở kết nối
+            _con.Open();
+            //3. Sau khi mở kết nối thì thực hiện 1 hành động nào đó
+            //4. Thực thi 1 câu Query với SqlCommand
+            _cmd = new SqlCommand("Select * from Accounts_ADO", _con);
+            //var data = _cmd.ExecuteReader();//Thực thi câu truy vấn
+            SqlDataAdapter data = new SqlDataAdapter(_cmd);
+            DataTable table = new DataTable();
+            data.Fill(table);//Đổ dữ liệu vào Table
+            dataGridView1.DataSource = table;
+            //5. Sau khi thực hiện xong hành động thì đóng kết nối
+        }
+    }
+}
